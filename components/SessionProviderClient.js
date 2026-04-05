d8854f3b5d553f76d8a479e381cd3501115ab460
nestrankapp <nestrankapp@gmail.com>
2026-04-05 19:58:10 -0400

    Create SessionProviderClient.js

---
+++ components/SessionProviderClient.js
@@ -0,0 +1,7 @@
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+export default function SessionProviderClient({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
