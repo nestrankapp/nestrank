bb287ffc7881070989183c091bbf064bf9aa3465
nestrankapp <nestrankapp@gmail.com>
2026-04-05 20:30:30 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,9 @@
 import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
+import GitHub from 'next-auth/providers/github';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: 'jwt' },
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET
-    })
-  ],
+  providers: [GitHub],
   pages: {
     signIn: '/signin'
   },
